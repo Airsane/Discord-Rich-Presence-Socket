357d811a402d900349ac0ba123a56d57976a9dde
Airsane <36139987+Airsane@users.noreply.github.com>
2021-07-16 14:04:46 +0200

    Update main.py

--- Client/main.py
+++ Client/main.py
@@ -7,7 +7,7 @@ HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = "" #Host IP
+SERVER = "" #Host IP example 192.168.0.22
 ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
